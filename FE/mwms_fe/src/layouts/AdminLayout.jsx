9a03ffcb3327ffc765eb6ab7f10c3a34d50abb60
KenTheWhale <kienquoc11026789@gmail.com>
2025-02-07 14:07:16 +0700

    feat: add layouts

---
+++ FE/mwms_fe/src/layouts/AdminLayout.jsx
@@ -0,0 +1,37 @@
+import {Outlet, useNavigate} from "react-router-dom";
+import style from "../styles/Layout.module.css";
+import {Menu, MenuItem, Sidebar} from "react-pro-sidebar";
+
+function GenerateNavbar() {
+    const navigate = useNavigate();
+    return (
+        <div className={`vw-17`}>
+            <h1 className={style.title}>ADMIN</h1>
+            <Sidebar collapsed={false} className={style.sidebar} backgroundColor={'#222222'}>
+                <Menu closeOnClick={true} className={style.menu}>
+                    <MenuItem onClick={() => {navigate("/admin/account")}}>Accounts</MenuItem>
+                </Menu>
+            </Sidebar>
+            <div className={style.profile}>
+                <img src={""} alt=""/>
+                <label>Account Name</label>
+            </div>
+            <div className={style.footer}>
+                <p>&copy; {new Date().getFullYear()} MWMS. All rights reserved</p>
+            </div>
+        </div>
+    )
+}
+
+export default function AdminLayout() {
+    return (
+        <div className={style.main}>
+            <div className={style.navbar_area}>
+                <GenerateNavbar/>
+            </div>
+            <div className={style.outlet_area}>
+                <Outlet/>
+            </div>
+        </div>
+    )
+}
