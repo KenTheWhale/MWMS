d2d2b8b820a72c605af8b00c076fbd14907abd9a
ViggaD <danhkvtse172932@gmail.com>
2025-02-07 18:32:03 +0700

    switch branch

--- FE/mwms_fe/src/App.jsx
+++ FE/mwms_fe/src/App.jsx
@@ -1,5 +1,19 @@
-function App() {
+import DashDefault from "./view/dashboard"
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
+
+function App() {
+   return(
+   <Router>
+    <Routes>
+        <Route path="/dashboard" element={<DashDefault/>}/>
+    </Routes>
+   </Router>
+   )
 }
 
 export default App
