7a7d64246e3dda34252f2153514d6e35a41285c2
KenTheWhale <kienquoc11026789@gmail.com>
2025-02-08 07:48:17 +0700

    fix:fix conflict

--- FE/mwms_fe/src/pages/LoginPage.jsx
+++ FE/mwms_fe/src/pages/LoginPage.jsx
@@ -1,3 +1,16 @@
+import {Button} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+
 export default function LoginPage() {
-    return <h1>Login</h1>
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <Button variant={"success"} onClick={() => {navigate("/manager")}}>Manager pages</Button>
+            <Button variant={"success"} onClick={() => {navigate("/staff")}}>Staff pages</Button>
+            <Button variant={"success"} onClick={() => {navigate("/admin")}}>Admin pages</Button>
+            <Button variant={"success"} onClick={() => {navigate("/sp")}}>Supplier pages</Button>
+            <Button variant={"success"} onClick={() => {navigate("/rq")}}>Requester pages</Button>
+        </>
+    )
 }
