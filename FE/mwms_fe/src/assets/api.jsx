368a65f7482a9e77f7fca06a53adbce5103495cd
ViggaD <thanhdanhvt2003@gmail.com>
2025-02-12 11:29:09 +0700

    iinteract BE with FE

---
+++ FE/mwms_fe/src/assets/api.jsx
@@ -0,0 +1,22 @@
+import axios from "axios"
+axios.defaults.baseURL = "http://localhost:8080/api/v1"
+
+const axiosClient = axios.create({
+    baseURL: axios.defaults.baseURL,
+    headers: {
+        "Content-Type" : "application/json"
+    },
+})
+
+axiosClient.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem("accessToken");
+        if(accessToken){
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+)
+
+export default axiosClient;
--- FE/mwms_fe/src/pages/LoginPage.jsx
+++ FE/mwms_fe/src/pages/LoginPage.jsx
@@ -1,16 +1,14 @@
-import {Button} from "react-bootstrap";
-import {useNavigate} from "react-router-dom";
+import Signin1 from "../view/auth/signin/SignIn1";
 
 export default function LoginPage() {
-    const navigate = useNavigate();
-
     return (
         <>
-            <Button variant={"success"} onClick={() => {navigate("/manager")}}>Manager pages</Button>
+            {/* <Button variant={"success"} onClick={() => {navigate("/manager")}}>Manager pages</Button>
             <Button variant={"success"} onClick={() => {navigate("/staff")}}>Staff pages</Button>
             <Button variant={"success"} onClick={() => {navigate("/admin")}}>Admin pages</Button>
             <Button variant={"success"} onClick={() => {navigate("/sp")}}>Supplier pages</Button>
-            <Button variant={"success"} onClick={() => {navigate("/rq")}}>Requester pages</Button>
+            <Button variant={"success"} onClick={() => {navigate("/rq")}}>Requester pages</Button> */}
+            <Signin1/>
         </>
     )
 }
