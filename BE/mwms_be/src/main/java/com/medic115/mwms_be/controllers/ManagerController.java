dcb4375ee8898021e188b6d40a896979832ee148
Ken_the_Whale <134859253+KenTheWhale@users.noreply.github.com>
2025-02-22 18:01:43 +0700

    Update ManagerController.java

--- BE/mwms_be/src/main/java/com/medic115/mwms_be/controllers/ManagerController.java
+++ BE/mwms_be/src/main/java/com/medic115/mwms_be/controllers/ManagerController.java
@@ -42,11 +42,6 @@ public class ManagerController {
 
     //-------------------------------------------------Task-------------------------------------------------//
 
-    @GetMapping("/task")
-    @PreAuthorize("hasAuthority('manager:read')")
-    public ResponseEntity<ResponseObject> getTaskList() {
-        return managerService.getTaskList();
-    }
 
     //-------------------------------------------------Request-------------------------------------------------//
 
