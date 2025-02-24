45714d0bb89593fc5f70481f6dd34d1744cdb07c
ViggaD <thanhdanhvt2003@gmail.com>
2025-02-24 07:43:57 +0700

    pull

--- BE/mwms_be/src/main/java/com/medic115/mwms_be/config/ApplicationConfig.java
+++ BE/mwms_be/src/main/java/com/medic115/mwms_be/config/ApplicationConfig.java
@@ -1,21 +1,17 @@
 package com.medic115.mwms_be.config;
 
 import com.medic115.mwms_be.repositories.AccountRepo;
-import com.medic115.mwms_be.services.UserService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @RequiredArgsConstructor
--- BE/mwms_be/src/main/java/com/medic115/mwms_be/controllers/AreaController.java
+++ BE/mwms_be/src/main/java/com/medic115/mwms_be/controllers/AreaController.java
@@ -5,6 +5,7 @@ import com.medic115.mwms_be.dto.response.AreaResponse;
 import com.medic115.mwms_be.services.AreaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,24 +17,28 @@ public class AreaController {
     private final AreaService areaService;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('manager:read')")
     public ResponseEntity<List<AreaResponse>> getAllAreas() {
         List<AreaResponse> response = areaService.getAllAreas();
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('manager:read')")
     public ResponseEntity<AreaResponse> getAreaById(@PathVariable Integer id) {
         AreaResponse response = areaService.getAreaById(id);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('manager:create')")
     public ResponseEntity<String> createArea(@RequestBody AreaRequest areaRequest) {
         areaService.createArea(areaRequest);
         return ResponseEntity.ok("created successfully !");
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('manager:update')")
     public ResponseEntity<AreaResponse> updateArea(@PathVariable Integer id, @RequestBody AreaRequest areaRequest) {
         AreaResponse response = areaService.updateArea(id, areaRequest);
         return ResponseEntity.ok(response);
