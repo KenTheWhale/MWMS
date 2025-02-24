bc136b3d40544748c7900e7a28509946744817c2
ViggaD <thanhdanhvt2003@gmail.com>
2025-02-24 19:27:36 +0700

    pull

--- BE/mwms_be/src/main/java/com/medic115/mwms_be/controllers/ManagerController.java
+++ BE/mwms_be/src/main/java/com/medic115/mwms_be/controllers/ManagerController.java
@@ -2,13 +2,17 @@ package com.medic115.mwms_be.controllers;
 
 import com.medic115.mwms_be.dto.requests.*;
 import com.medic115.mwms_be.dto.response.AddCategoryResponse;
+import com.medic115.mwms_be.dto.response.AreaResponse;
 import com.medic115.mwms_be.dto.response.ViewCategoryResponse;
 import com.medic115.mwms_be.dto.response.ResponseObject;
+import com.medic115.mwms_be.services.AreaService;
 import com.medic115.mwms_be.services.ManagerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -17,6 +21,8 @@ import org.springframework.web.bind.annotation.*;
 public class ManagerController {
 
     private final ManagerService managerService;
+
+    private final AreaService areaService;
 
     //-------------------------------------------------Category-------------------------------------------------//
 
@@ -104,6 +110,36 @@ public class ManagerController {
     @PreAuthorize("hasRole('manager')")
     public ResponseEntity<ResponseObject> getAllUnassignedGroup() {
         return managerService.getAllUnassignedGroup();
+    }
+
+
+    //-------------------------------------------------Area-----------------------------------------------------//
+    @GetMapping("/area")
+    @PreAuthorize("hasRole('manager')")
+    public ResponseEntity<List<AreaResponse>> getAllAreas() {
+        List<AreaResponse> response = areaService.getAllAreas();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/area/{id}")
+    @PreAuthorize("hasRole('manager')")
+    public ResponseEntity<AreaResponse> getAreaById(@PathVariable Integer id) {
+        AreaResponse response = areaService.getAreaById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/area")
+    @PreAuthorize("hasRole('manager')")
+    public ResponseEntity<String> createArea(@RequestBody AreaRequest areaRequest) {
+        areaService.createArea(areaRequest);
+        return ResponseEntity.ok("created successfully !");
+    }
+
+    @PutMapping("/area/{id}")
+    @PreAuthorize("hasRole('manager')")
+    public ResponseEntity<AreaResponse> updateArea(@PathVariable Integer id, @RequestBody AreaRequest areaRequest) {
+        AreaResponse response = areaService.updateArea(id, areaRequest);
+        return ResponseEntity.ok(response);
     }
 
 }
