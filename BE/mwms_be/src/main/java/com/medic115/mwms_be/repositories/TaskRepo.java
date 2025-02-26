7bd2ec65490bde5e79ee1445f70d419e1c8cf7b2
HuyTran0211 <quochuy2112002@gmail.com>
2025-02-26 10:34:08 +0700

    init data

--- BE/mwms_be/src/main/java/com/medic115/mwms_be/repositories/TaskRepo.java
+++ BE/mwms_be/src/main/java/com/medic115/mwms_be/repositories/TaskRepo.java
@@ -4,4 +4,5 @@ import com.medic115.mwms_be.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepo extends JpaRepository<Task, Integer> {
+    boolean existsByCode(String code);
 }
