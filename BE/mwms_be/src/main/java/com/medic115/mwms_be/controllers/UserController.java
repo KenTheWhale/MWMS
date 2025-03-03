f63ba1ca76a410935c137e892678af9522abe6d1
ViggaD <thanhdanhvt2003@gmail.com>
2025-03-03 21:29:33 +0700

    pull

--- BE/mwms_be/src/main/java/com/medic115/mwms_be/controllers/UserController.java
+++ BE/mwms_be/src/main/java/com/medic115/mwms_be/controllers/UserController.java
@@ -2,11 +2,14 @@ package com.medic115.mwms_be.controllers;
 
 import com.medic115.mwms_be.dto.requests.RefreshTokenRequest;
 import com.medic115.mwms_be.dto.response.JwtAuthenticationResponse;
+import com.medic115.mwms_be.dto.response.UserResponse;
 import com.medic115.mwms_be.services.AuthenticationService;
+import com.medic115.mwms_be.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -15,10 +18,17 @@ public class UserController {
 
     private final AuthenticationService authenticationService;
 
+    private final UserService userService;
+
 
     @PostMapping("/refresh")
     @PreAuthorize("hasRole('admin') || hasRole('manager') || hasRole('staff') || hasRole('partner')")
     ResponseEntity<JwtAuthenticationResponse> refreshToken(@RequestBody RefreshTokenRequest request) {
         return authenticationService.refreshToken(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllAccountExceptAdmin());
     }
 }
