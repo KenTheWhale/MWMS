b46961e032a1df161739979a630538b9556e75da
ViggaD <thanhdanhvt2003@gmail.com>
2025-03-04 17:09:04 +0700

    implement signup for authController

--- BE/mwms_be/src/main/java/com/medic115/mwms_be/controllers/AuthController.java
+++ BE/mwms_be/src/main/java/com/medic115/mwms_be/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.medic115.mwms_be.controllers;
 
 
 import com.medic115.mwms_be.dto.requests.SignInRequest;
+import com.medic115.mwms_be.dto.requests.SignUpRequest;
 import com.medic115.mwms_be.dto.response.JwtAuthenticationResponse;
 import com.medic115.mwms_be.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
@@ -21,5 +22,10 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<JwtAuthenticationResponse> signIn(@RequestBody SignInRequest request) {
         return authenticationService.signIn(request);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> signUp(@RequestBody SignUpRequest request) {
+        return authenticationService.signUp(request);
     }
 }
